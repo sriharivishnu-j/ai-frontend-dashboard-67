b2d35a46a74768a0c6ad0ecae971e81fa6cbaf99
Sriharivishnu Janakiraman <j.sriharivishnu@gmail.com>
2026-01-12 23:39:38 +0800

    Add src/pages/api/trpc/[trpc].ts

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,7 @@
+import { createNextApiHandler } from '@trpc/server/adapters/next';
+import { appRouter } from '../../../server/routers/_app';
+
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: () => ({}),
+});
